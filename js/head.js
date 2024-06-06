62fa1abfdac95bbb1d97e2a3a0e99281cd9edb23
Minrry <pereskokova2004@gmail.com>
2024-06-06 23:08:46 +0400

    Update head.js

--- js/head.js
+++ js/head.js
@@ -1,24 +1,26 @@
+// script.js
+
+// Добавление тени к заголовку при прокрутке
 const header = document.querySelector("header");
-window.addEventListener("scroll" , () => {
-        header.classList.toggle("shadow", window.scrollY > 40);
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 40);
 });
 
-
-// адаптивное меню
+// Адаптивное меню
 const menuBtn = document.querySelector('.menu_btn');
 const menu = document.querySelector('.menu');
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     menu.classList.toggle("active");
-})
+});
 
 document.querySelectorAll(".menu_lin").forEach(n => n.addEventListener("click", () => {
     menuBtn.classList.remove("active");
     menu.classList.remove("active");
-}))
+}));
 
-
+// Подсказки
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.tooltip').forEach(tooltip => {
         tooltip.addEventListener('click', function (event) {
@@ -41,5 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
 
 
